665f48a19fa1f6f10f1abe68a03e03abecf1b9e8
Levon90210 <levonshahbazyan10@gmail.com>
2025-05-10 18:29:06 +0400

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/levon/CLionProjects/Algorithms/Graph
-# Build directory: /home/levon/CLionProjects/Algorithms/Graph/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
